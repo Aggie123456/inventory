85ab0148ec21f87e486bc79fa4b0bd41bc71a359
Aggie123456 <lwensisiagnes@gmail.com>
2025-02-04 21:16:52 +0300

    Add files via upload

---
+++ JAVASCRIPT/Script.js
@@ -0,0 +1,15 @@
+//checking for inventory
+let phones = {
+    'pop':['Pop 8(64gb)','Pop 9(64gb)','Pop 9(128gb)'],
+    'Spark':['Spark 30C','Spark 30(128gb)','Spark 30(256gb)'],
+    'Camon':['Camon 30s','Camon 30s pro','Camon 30(256gb)','Camon 30(512gb)'],
+    'Smart':['Smart 8','Smart 9(64gb)','Smart 9(128gb'],
+    'hot':['Hot 40(128gb)','Hot 40(256gb)','Hot 50i(128gb)','Hot 50pro plus(128gb','Hot 50pro plus(256gb)'],
+    'note':['Note 40(256gb)','Note 50(512gb)','Note 40pro plus'],
+};
+console.log(phones.pop);
+console.log(phones.Spark);
+console.log(phones.camon);
+console.log(phones.Smart);
+console.log(phones.hot);
+console.log(phones.note);
